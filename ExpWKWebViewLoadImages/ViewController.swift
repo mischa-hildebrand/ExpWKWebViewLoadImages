9fb427560814dbfbf6783e9d579fd74769b47283
Mischa Hildebrand <mischa.hildebrand@kupferwerk.com>
2016-10-06 12:48:44 +0200

    Initial Commit

---
+++ ExpWKWebViewLoadImages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpWKWebViewLoadImages
+//
+//  Created by Mischa Hildebrand on 06/10/16.
+//  Copyright © 2016 Kupferwerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
