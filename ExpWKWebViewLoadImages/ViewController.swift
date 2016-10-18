c9d46f8ad156d2402c9066a4cd186057697ea929
Mischa Hildebrand <mischa.hildebrand@kupferwerk.com>
2016-10-18 12:08:13 +0200

    Add buttons to load WebView with different contents / baseURLs.

--- ExpWKWebViewLoadImages/ViewController.swift
+++ ExpWKWebViewLoadImages/ViewController.swift
@@ -10,65 +10,113 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
+    
+    enum BaseURLTarget {
+        
+        // The App's Bundle folder
+        case bundleRootFolder
+        // The App's Library folder
+        case libraryFolder
+        
+        var url: URL? {
+            switch self {
+                
+            case .bundleRootFolder:
+                return Bundle.main.bundleURL
+                
+            case .libraryFolder:
+                do {
+                    return try FileManager.default.url(for: FileManager.SearchPathDirectory.libraryDirectory,
+                                                       in: FileManager.SearchPathDomainMask.userDomainMask,
+                                                       appropriateFor: nil,
+                                                       create: false)
+                } catch {
+                    return nil
+                }
+            }
+        }
+    }
+    
+    // MARK: - Instance Members
+    
+    @IBOutlet weak var buttonControlView: UIView!
 
     let webView = WKWebView()
+    
+    let websiteURL = URL(string: "http://stackoverflow.com/q/39901982/2062785")
     
     let imageURL = URL(string: "https://cdn.sstatic.net/Sites/stackoverflow/company/img/logos/so/so-icon.png")
     
     var imageName: String {
-        return "image" + (imageURL?.lastPathComponent)!
-    }
-    
-    let htmlName = "htmlFile.html"
-    
-    var htmlURL: URL {
-        return libraryURL.appendingPathComponent(htmlName)
+        return imageURL!.lastPathComponent
     }
     
     var htmlString: String {
         return "<html><head><title>Testing images in a WKWebView</title></head><body style=\"font-size: 2rem\"><h1>This is the Heading</h1><p>... and this is a paragraph. Both are contained in an HTML string that is loaded inside the <b>WKWebView</b> using its <b>loadHTMLString(String, baseURL: URL?)</b> method.</p><p>In the following line an image should be displayed:</p><p><img src=\"\(imageName)\" /></p><p>For some mysterious reason, it works in Simulator but it doesn't on a real device. So sad. :(</p></body></html>"
     }
     
-    let libraryURL = try! FileManager.default.url(for: FileManager.SearchPathDirectory.libraryDirectory,
-                                                 in: FileManager.SearchPathDomainMask.userDomainMask,
-                                                 appropriateFor: nil,
-                                                 create: false)
+    // MARK: - Initial Setup
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        addWebView()
-        
-        try! htmlString.write(to: htmlURL, atomically: true, encoding: .utf8)
-        
+
+        view.addSubview(webView)
         try! downloadAndStoreImageInLibraryFolder()
-        
-        webView.loadFileURL(htmlURL, allowingReadAccessTo: htmlURL.deletingLastPathComponent())
-        
-        //webView.loadHTMLString(htmlString, baseURL: libraryURL)
     }
     
-    func addWebView() {
-        view.addSubview(webView)
-        webView.frame = view.bounds.insetBy(dx: 0, dy: 20)
+    override func viewDidLayoutSubviews() {
+        // Position and size webView properly
+        var frame = view.bounds
+        frame.origin.y += 20 // Avoid overlap with status bar
+        frame.size.height -= buttonControlView.frame.size.height // Don't cover button control view
+        webView.frame = frame
     }
+    
+    // MARK: - Button Actions
+    
+    @IBAction func loadFromWebButtonTapped(_ sender: AnyObject) {
+        loadWebsite()
+    }
+    
+    @IBAction func loadFromBundleButtonTapped(_ sender: AnyObject) {
+        loadLocalHTMLString(with: .bundleRootFolder)
+    }
+    
+    @IBAction func loadFromLibraryButtonTapped(_ sender: AnyObject) {
+        loadLocalHTMLString(with: .libraryFolder)
+    }
+    
+    // MARK: - Load Webview Content
+    
+    func loadWebsite() {
+        print("Loading Webview with URL: \(websiteURL)\n")
+        let request = URLRequest(url: websiteURL!)
+        webView.load(request)
+    }
+    
+    func loadLocalHTMLString(with baseURLTarget: BaseURLTarget) {
+        print("Loading Webview with baseURL: \(baseURLTarget.url)\n")
+        print("--- Contents of directory: ---\n")
+        printContentsOfDirectory(at: baseURLTarget.url)
+        webView.loadHTMLString(htmlString, baseURL: baseURLTarget.url)
+    }
+    
+    // MARK: - Helpers
     
     func downloadAndStoreImageInLibraryFolder() throws {
         let data = try Data(contentsOf: imageURL!)
-        let targetImageURL = libraryURL.appendingPathComponent(imageName)
-        try data.write(to: targetImageURL)
+        let targetImageURL = BaseURLTarget.libraryFolder.url?.appendingPathComponent(imageName)
+        if let targetImageURL = targetImageURL {
+            try data.write(to: targetImageURL)
+        }
     }
     
-    func loadWebsite() {
-        let request = URLRequest(url: URL(string: "https://www.google.de")!)
-        webView.load(request)
+    func printContentsOfDirectory(at URL: URL?) {
+        let contents = try! FileManager.default.contentsOfDirectory(at: URL!, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        for item in contents {
+            print("\t", item.resolvingSymlinksInPath(), "\n")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
